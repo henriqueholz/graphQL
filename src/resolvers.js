331d4fcab892dc7dfcf96d045191e76d3eedf7ef
henrique.castro <henriqueholz@live.com>
2019-12-07 17:35:05 -0500

    users

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,22 +1,20 @@
-const users = [{
-  id: 1,
-  name: "Thiago",
-  email: "tgmarinho@gmail.com"
-},
-{
-  id: 2,
-  name: "Diego",
-  email: "diego@rocketseat.com"
-}
-];
+const User = require("./User");
 
 module.exports = {
   Query: {
-    users: () => users,
-    user: () => users[0]
+    users: () => User.find(),
+    user: (root, {
+      id
+    }) => User.findById(id)
   },
 
   Mutation: {
-    createUser: () => users[1]
+    createUser: (root, {
+      name,
+      email
+    }) => User.create({
+      name,
+      email
+    })
   }
 };
