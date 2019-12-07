6da24eba9ddaa2ef9059e8c0015694cc049b80a2
henrique.castro <henriqueholz@live.com>
2019-12-07 17:25:41 -0500

    mongoose

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,10 +1,22 @@
+const users = [{
+  id: 1,
+  name: "Thiago",
+  email: "tgmarinho@gmail.com"
+},
+{
+  id: 2,
+  name: "Diego",
+  email: "diego@rocketseat.com"
+}
+];
+
 module.exports = {
   Query: {
-    users: () => { },
-    user: () => { }
+    users: () => users,
+    user: () => users[0]
   },
 
   Mutation: {
-    createUser: () => { }
+    createUser: () => users[1]
   }
 };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
-import { GraphQLServer } from "graphql-yoga";
+const { GraphQLServer } = require("graphql-yoga");
+const path = require("path");
+const resolvers = require("./resolvers");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/graphqlnode", {
+  useNewUrlParser: true
+});
 
 const server = new GraphQLServer({
-
+  typeDefs: path.resolve(__dirname, "schema.graphql"),
+  resolvers
 });
 
 server.start();
